46c7ebcf0aca0778934deb66bf1f8c9c6f2d19ab
Илья <qq.ferveks@gmail.com>
2020-03-18 16:43:43 +0300

    0. Операции сравнения[#250267]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -1,20 +1,20 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(double heigth) {
-        double rsl = (heigth - 100) * 1.15;
+    public static double manWeight(double height) {
+        double rsl = (height - 100) * 1.15;
         return rsl;
     }
 
-    public static double woomanWeigth(double heigth) {
+    public static double womanWeight(double heigth) {
         double rsl = (heigth - 110) * 1.15;
         return rsl;
     }
 
     public static void main(String[] args) {
-        double man = Fit.manWeight(170);
-        double wooman = Fit.woomanWeigth(170);
-        System.out.println("Man 100 is " + man );
-        System.out.println("wooman 100 is " + wooman);
+        double man = Fit.manWeight(180);
+        double woman = Fit.womanWeight(160);
+        System.out.println("Man 180 is " + man );
+        System.out.println("woman 160 is " + woman);
     }
 }
