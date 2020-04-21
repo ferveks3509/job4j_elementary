d33fd495087b907eabd5dd59ec47a487e35b92f4
Илья <qq.ferveks@gmail.com>
2020-04-21 17:52:24 +0300

    6.2.3. Перевернуть массив.[#250293]

--- src/ru/job4j/array/TurnSting.java
+++ src/ru/job4j/array/TurnSting.java
@@ -2,19 +2,19 @@ package ru.job4j.array;
 
 public class TurnSting {
     public static void main(String[] args) {
-        String[] q = new String[] { "Каждый", "охотник", "хочет", "знать", "где", "фазан"};
-        for(int i = 0; i < q.length; i++) {
-            System.out.println(q[i]);
+        String[] array = new String[] { "Каждый", "охотник", "хочет", "знать", "где", "фазан"};
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
-        int a = q.length;
+        int a = array.length;
         for(int i = 0; i < a / 2; i++) {
-            String temp = q[a - i - 1];
-            q[a - i - 1] = q[i];
-            q[i] = temp;
+            String temp = array[a - i - 1];
+            array[a - i - 1] = array[i];
+            array[i] = temp;
         }
         System.out.println();
-        for(int i = 0; i < q.length; i++) {
-            System.out.println(q[i]);
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
 
     }
