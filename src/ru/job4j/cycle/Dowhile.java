4a96ff8262f914be1f74e7116d9024031d1aaf4f
Илья <qq.ferveks@gmail.com>
2020-05-02 18:19:25 +0300

    cycle DoWhile

---
+++ src/ru/job4j/cycle/Dowhile.java
@@ -0,0 +1,9 @@
+package ru.job4j.cycle;
+
+public class Dowhile {
+    public static void main(String[] args) {
+        do {
+            System.out.println("yes");
+        } while (1 == 2);
+    }
+}
