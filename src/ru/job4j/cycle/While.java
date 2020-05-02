85546082d4f6d1fcd500c552cd7926438c505b3c
Илья <qq.ferveks@gmail.com>
2020-05-02 18:15:22 +0300

    cycle While

---
+++ src/ru/job4j/cycle/While.java
@@ -0,0 +1,11 @@
+package ru.job4j.cycle;
+
+public class While {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a < 6) {
+            a++;
+            System.out.println(a);
+        }
+    }
+}
