7706cb711f8c978ffff2366ab373b431a17afca4
Илья <qq.ferveks@gmail.com>
2020-05-08 15:52:03 +0300

    2.2. Рефакторинг - Площадь треугольника.[#250442]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,10 +1,34 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
-        if ( ab + ac > bc && ac + bc > ab && ab + bc > ac) {
-            return true;
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+    public boolean exist(double ab, double ac, double bc) {
+        boolean rsl = (ab + ac) > bc && (ac + bc) > ab && (ab + bc) > ac;
+        return rsl;
+    }
+
+    public double period(double a, double b, double c) {
+        double itog = (a + b + c) / 2;
+        return itog;
+    }
+
+    public double area() {
+        double rs1 = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rs1 = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         }
-        return false;
+        return rs1;
     }
 }
