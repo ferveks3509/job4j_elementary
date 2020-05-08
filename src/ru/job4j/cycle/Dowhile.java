a891a3cea654c413736b4068fa2459fcdafc3592
Илья <qq.ferveks@gmail.com>
2020-05-08 22:04:22 +0300

    5.1. Зона видимости переменных[#250452]

--- src/ru/job4j/cycle/Dowhile.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.cycle;
-
-public class Dowhile {
-    public static void main(String[] args) {
-        do {
-            System.out.println("yes");
-        } while (1 == 2);
-    }
-}
