5ce3cc3c1f63d8bf195a686210e2154c226399ec
Илья <qq.ferveks@gmail.com>
2020-05-12 17:27:42 +0300

    Тернарное сравнение[#261564]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,12 +2,15 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int first, int second) {
-        return first > second ? first : second;
+        int itog = first > second ? first : second;
+        return itog;
     }
-    public static int max(int first, int second, int third) {
-        return max(first, max(second, third));
+    public static int max1(int two, int three) {
+        int itog = two > three ? two : three;
+        return itog;
     }
-    public static int max(int first, int second, int third, int fourth) {
-        return max(first, max(second, third, fourth));
+    public static int max2(int four, int five) {
+        int itog = four > five ? four : five;
+        return itog;
     }
 }
