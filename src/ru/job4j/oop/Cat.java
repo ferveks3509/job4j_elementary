4606d47dbf36cea156547e89155b7d8d4a09a95c
Илья <qq.ferveks@gmail.com>
2020-05-12 13:00:40 +0300

    1.5. Поля объекта[#250446]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -2,20 +2,26 @@ package ru.job4j.oop;
 
 public class Cat {
     private String food; //создаем поле объекта
+    private String name;
 
     public void eat(String meat) { //инициализируем поле объекта
         this.food = meat;
     }
     public void show() { //метод для вывода поля в консоль
-        System.out.println(this.food);
+        System.out.println(this.food + " - " + this.name);
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
     }
     public static void main(String[] args) {
         System.out.println("There are food for Peppy:");
         Cat peppy = new Cat();
-        peppy.eat("kotleta"); // если не указать то умолчанию, во всех полях объекта записываются значения по умолчанию String - Null
+        peppy.giveNick("for belka");
+        peppy.eat("kotleta "); // если не указать то умолчанию, во всех полях объекта записываются значения по умолчанию String - Null
         peppy.show();  // вызов метода указанием объекта . обращение к методу
         System.out.println("There are food for Sparky:");
         Cat sparky = new Cat();
+        sparky.giveNick("for strelka");
         sparky.eat("fish"); // если не указать то умолчанию, во всех полях объекта записываются значения по умолчанию String - Null
         sparky.show();
     }
