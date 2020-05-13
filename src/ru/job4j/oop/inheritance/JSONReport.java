146077217672ffec7f9fb55cb5583cd6ff5c5394
Илья <qq.ferveks@gmail.com>
2020-05-13 17:23:09 +0300

    4. Переопределение[#250458]

---
+++ src/ru/job4j/oop/inheritance/JSONReport.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return   "{" + System.lineSeparator() + "name: " +  name
+                + System.lineSeparator() + "body " + body + System.lineSeparator() + "}";
+    }
+
+    public static void main(String[] args) {
+        JSONReport one = new JSONReport();
+        String rsl = one.generate("project", "text");
+        System.out.println(rsl);
+    }
+}
