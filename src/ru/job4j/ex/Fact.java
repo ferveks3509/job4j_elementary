670341ea0b91fda10ba075cce46e500666795a81
Илья <qq.ferveks@gmail.com>
2020-06-06 17:02:49 +0300

    0.1. java.lang.NullPointerException[219364#250423]

---
+++ src/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n should be bigger 0.");
+        }
+        int rsl = 1;
+        for (int i = 1; i < n; i++) {
+            rsl *= i;
+        }
+        return rsl;
+    }
+    public static void main(String[] args) {
+        Fact fact = new Fact();
+        System.out.println(fact.calc(-1));
+    }
+}
